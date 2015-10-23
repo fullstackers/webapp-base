cbab217409a4fb004f19c0a7167cde64eac08eb7
Paul Forbes <paulforbes42@gmail.com>
2015-10-23 11:13:24 -0400

    Added ngdoc to ServiceTemplate, Implemented new pattern for Services

--- app/js/service/ServiceTemplate.js
+++ app/js/service/ServiceTemplate.js
@@ -1,17 +1,32 @@
 (function(ng) {
     "use strict";
-    
+
+    /**
+     * @ngdoc service
+     * @name app.service:TestService
+     * @description
+     * The `TestService` represents a template for creating services within webapp-base
+     */    
     ng
         .module('app')
         .factory('TestService', [function() {
             var title = "Test";
 
-            function _getTitle() {
-                return title;
-            }
-
+            // Return the service object at the top of the service. Then,
+            // define the functions for the service below the return statement
             return {
                 getTitle: _getTitle
             };
+
+            /**
+             * @ngdoc method
+             * @name getTitle
+             * @methodOf app.service:TestService
+             * @description
+             * Get the title stored in the `TestService`
+             */
+            function _getTitle() {
+                return title;
+            }
         }]);
 })(angular);
