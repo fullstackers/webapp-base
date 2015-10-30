40156d390ce369e031fb86d17b13981daad9ce2b
Paul Forbes <paulforbes42@gmail.com>
2015-10-30 19:20:35 +0000

    Scroll to the top of the page each time a new page loads.

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,13 @@
 
     ng
         .module('app', ['ui.router', 'templates-generated'])
+        .run(['$rootScope', function($rootScope) {
+        
+            // Scroll to the top of the page each time a new page loads
+            $rootScope.$on('$stateChangeSuccess', function() {
+                document.body.scrollTop = document.documentElement.scrollTop = 0;
+            });
+        }])
         .config(['$urlRouterProvider', function($urlRouterProvider) {
             $urlRouterProvider.otherwise('/dashboard');
         }]);
