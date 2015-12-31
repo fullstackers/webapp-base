1dfaffd6b1fa237404b7c9734cb7a8b908707279
Paul Forbes <paulforbes42@gmail.com>
2015-12-31 14:28:23 -0500

    Added gzip compression

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 var path = require('path'),
     express = require('express'),
+    compression = require('compression'),
     app = express(),
     port = process.env.PORT || 3000,
     staticDirectory = process.env.NODE_ENV === 'production' ? 'dist' : 'generated',
     staticPath = path.join(__dirname, staticDirectory);
 
+// gzip static content
+app.use(compression());
+
+// serve static content
 app.use(express.static(staticPath));
 
+// Support HTML5 Mode for the AngularJS app
 app.all('/*', function(req, res) {
     res.sendFile(staticPath + '/index.html');
 });
 
+// Start the HTTP Server
 app.listen(port, function() {
     console.log('Web Application Static Server Started');
     console.log('Port:', port);
