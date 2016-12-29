11d441f37a50a93ed5a085aef807f212a378b519
Ho Kai Yip <ho.kai.yip@fullstackers.com>
2016-12-29 00:14:54 -0500

    Fullstackers e2e tests

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -1,11 +1,11 @@
 describe("homepage", function () {
   it("should show proper greeting", function () {
     browser.get("http://localhost:3001/dashboard");
-    element(by.model('yourName')).sendKeys('Julie');
+    element(by.model('yourName')).sendKeys('Fullstackers');
 
     var greeting = element(by.binding('yourName'));
 
-    expect(greeting.getText()).toEqual('Hello Julie!');
+    expect(greeting.getText()).toEqual('Hello Fullstackers!');
   });
 
 });
